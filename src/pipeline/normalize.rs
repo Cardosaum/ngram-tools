aea6aa208be7dd80cbf3644dadac1418cf60a3e7
Cardosaum <mcardosos99@gmail.com>
2020-08-28 16:38:11 -0300

    modified wordsep, phrasesep and chartype to accept "'" as word constituent

--- src/pipeline/normalize.rs
+++ src/pipeline/normalize.rs
@@ -4,7 +4,7 @@ use std::fmt;
 enum CharType {
     Letter(u8),
     WordSep,
-    PhraseSep,
+    // PhraseSep,
     SentenceSep,
     Join,
     Skip,
@@ -15,7 +15,7 @@ impl fmt::Debug for CharType {
         match self {
             CharType::Letter(a) => write!(f, "Letter({} ({}))", *a as char, a),
             CharType::WordSep => write!(f, "WordSep"),
-            CharType::PhraseSep => write!(f, "PhraseSep"),
+            // CharType::PhraseSep => write!(f, "PhraseSep"),
             CharType::SentenceSep => write!(f, "SentenceSep"),
             CharType::Join => write!(f, "Join"),
             CharType::Skip => write!(f, "Skip"),
@@ -30,9 +30,9 @@ pub fn normalize_ascii(text: &Vec<u8>, keep_commas: bool) -> Vec<u8> {
     for this_char in text.iter() {
         t2 = match this_char {
             b'A'...b'Z' => CharType::Letter(*this_char + 32),
-            b'a'...b'z' => CharType::Letter(*this_char),
-            b' ' | b'\t' | b'\n' | b'"' | b'(' | b')' => CharType::WordSep,
-            b',' => CharType::PhraseSep,
+            b'a'...b'z' | b'\'' | b'0' | b'1' | b'2' | b'3' | b'4' | b'5' | b'6' | b'7' | b'8' | b'9' => CharType::Letter(*this_char),
+            b' ' | b'\t' | b'\n' | b'"' | b'(' | b')' | b',' => CharType::WordSep,
+            // b',' => CharType::PhraseSep,
             b'!' | b'?' | b'.' | b';' | b':' => CharType::SentenceSep,
             b'-' => CharType::Join,
             _ => CharType::Skip,
@@ -61,23 +61,24 @@ pub fn normalize_ascii(text: &Vec<u8>, keep_commas: bool) -> Vec<u8> {
             (CharType::WordSep, CharType::SentenceSep) => {
                 t1 = CharType::SentenceSep;
             }
-            (CharType::PhraseSep, CharType::Letter(_)) => {
-                t1 = t2;
-                if keep_commas {
-                    write_buffer.push(b' ');
-                    write_buffer.push(b',');
-                    write_buffer.push(b' ');
-                }
-            }
-            (CharType::PhraseSep, CharType::SentenceSep) => {
-                t1 = t2;
-            }
+            // (CharType::PhraseSep, CharType::Letter(_)) => {
+            //     t1 = t2;
+            //     if keep_commas {
+            //         write_buffer.push(b' ');
+            //         write_buffer.push(b',');
+            //         write_buffer.push(b' ');
+            //     }
+            // }
+            // (CharType::PhraseSep, CharType::SentenceSep) => {
+            //     t1 = t2;
+            // }
             (CharType::SentenceSep, CharType::Letter(_)) => {
                 t1 = t2;
                 write_buffer.push(b'\n');
             }
             (CharType::Join, CharType::Letter(_)) => {
                 t1 = t2;
+                write_buffer.push(b' ');
             }
             (_, _) => (),
         }
